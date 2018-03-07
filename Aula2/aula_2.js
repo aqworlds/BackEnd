45fad5d6dda5beaa9ece4b6653562263ef17b3c2
aqworlds <32848862+aqworlds@users.noreply.github.com>
2018-03-07 21:42:52 +0000

    commit

--- Aula2/aula_2.js
+++ Aula2/aula_2.js
@@ -1,6 +1,31 @@
 // exercicio1
 function mCorporal(peso,altura){
-    imc = peso/(altura)^2;
-    return imc
+    return peso/(altura)^2;
 }
-mCorporal(2,3);
+function massaC(){
+    peso= 60;
+    altura= 1.90;
+    imc = mCorporal(peso,altura);
+    if (imc < 18.5){
+        return ("A sua massa corporal esta a baixo dos limites" + ' ' + imc);
+    }
+    if(imc >= 18.5 && imc <= 25){
+        return ("A sua massa corporal esta normal" + ' ' + imc);
+    }
+    if(imc >25 && imc <= 30 ){
+        return ("A sua massa corporal esta acima do peso" + ' ' + imc)
+    }
+    else{
+        return ("A sua massa corporal mostra que esta obeso" + ' ' + imc)
+    }
+}
+// exercicio2
+
+function inverte(str) {
+    if (str == "")
+      return "";
+    else
+      return inverte(str.substr(1)) + str.charAt(0);
+  }
+console.log(inverte("Hoje e domingo"));
+//inline
